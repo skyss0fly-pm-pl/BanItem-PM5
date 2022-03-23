56ba56c5966129b44f0c14f25f7595f98d91dba8
YTBJero <nguyenhieucoder47@gmail.com>
2022-03-23 15:22:28 +0700

    Added Support API 4

--- src/LDX/BanItem/Main.php
+++ src/LDX/BanItem/Main.php
@@ -2,9 +2,13 @@
 
 namespace LDX\BanItem;
 
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\item\Armor;
+use pocketmine\item\Sword;
+use pocketmine\item\Tool;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\event\player\PlayerInteractEvent;
@@ -19,20 +23,20 @@ class Main extends PluginBase implements Listener {
 
     private $spys = [];
 
-    public function onEnable() {
+    public function onEnable(): void {
         $this->saveItems();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function getPlayerPosition(Player $sender) {
 
-        $playerX = $sender->getX();
-        $playerY = $sender->getY();
-        $playerZ = $sender->getZ();
+        $playerX = $sender->getPosition()->getX();
+        $playerY = $sender->getPosition()->getY();
+        $playerZ = $sender->getPosition()->getZ();
         $outX = round($playerX, 1);
         $outY = round($playerY, 1);
         $outZ = round($playerZ, 1);
-        $playerLevel = $sender->getLevel()->getName();
+        $playerLevel = $sender->getWorld()->getFolderName();
         return ("x:" . $outX . ", y:" . $outY . ", z:" . $outZ . ". On: " . $playerLevel);
     }
 
@@ -49,7 +53,24 @@ class Main extends PluginBase implements Listener {
             }
             if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass"))) {
                 $p->sendMessage("[BanItem] That item is banned.");
-                $event->setCancelled(true);
+                $event->cancel();
+            }
+        }
+    }
+
+    public function onTap(PlayerItemUseEvent $event){
+        if ($event->isCancelled()) return;
+        $p = $event->getPlayer();
+        if($this->isBanned($event->getItem())) {
+            $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getItem() . " at " . $this->getPlayerPosition($p));
+            foreach($this->getServer()->getOnlinePlayers() as $player) {
+                if(isset($this->spys[strtolower($player->getName())])) {
+                    $player->sendMessage($p->getName() . " tried a Banned Item: " . $event->getItem());
+                }
+            }
+            if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass"))) {
+                $p->sendMessage("[BanItem] That item is banned.");
+                $event->cancel();
             }
         }
     }
@@ -65,7 +86,7 @@ class Main extends PluginBase implements Listener {
                 }
             }
             if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass"))) {
-                $event->setCancelled(true);
+                $event->cancel();
             }
         }
     }
@@ -82,7 +103,7 @@ class Main extends PluginBase implements Listener {
                 }
                 if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass"))) {
                     $p->sendMessage("[BanItem] That item is banned.");
-                    $event->setCancelled(true);
+                    $event->cancel();
                 }
             }
         }
@@ -100,7 +121,7 @@ class Main extends PluginBase implements Listener {
             }
             if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass"))) {
                 $p->sendMessage("[BanItem] That item is banned.");
-                $event->setCancelled(true);
+                $event->cancel();
             }
         }
     }
@@ -118,7 +139,7 @@ class Main extends PluginBase implements Listener {
                 $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getItem() . " at " . $this->getPlayerPosition($p));
                 if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass"))) {
                     $p->sendMessage("[BanItem] That item is banned.");
-                    $event->setCancelled(true);
+                    $event->cancel();
                 }
             }
         }
@@ -185,9 +206,16 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function isBanned($i) {
-        if(in_array(strval($i->getID()), $this->items, true) || in_array(($i->getID() . "#" . $i->getDamage()), $this->items, true)) {
-            return true;
+    public function isBanned($i)
+    {
+        if ($i instanceof Tool || $i instanceof Armor || $i instanceof Sword){
+            if (in_array(strval($i->getID()), $this->items, true) || in_array(($i->getID() . "#" . $i->getDamage()), $this->items, true)) {
+                return true;
+            }
+        } else{
+            if (in_array(strval($i->getID()), $this->items, true) || in_array(($i->getID() . "#" . $i->getMeta()), $this->items, true)) {
+                return true;
+            }
         }
         return false;
     }
